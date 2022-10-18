8c891b7fcf2b84bd51a9bebba02233c49e870eca
Nafees Ahmad <nafees.ahmad@tothenew.com>
2022-10-19 00:01:34 +0530

    update

--- Ques1.java
+++ Ques1.java
@@ -1,10 +1,24 @@
 /*
-(1) Write a program to demonstrate Tightly Coupled code.
+    (1) Write a program to demonstrate Tightly Coupled code.
  */
 
 package SpringSession1;
 
+
+// Class Ques1 is dependent on Car class. That's why it is tightly coupled
 public class Ques1
 {
+    Car carObj = new Car();
+    public void startJourney()
+    {
+        carObj.travel();
+    }
+}
 
+class Car
+{
+    public void travel()
+    {
+        System.out.println("Travel by car");
+    }
 }
